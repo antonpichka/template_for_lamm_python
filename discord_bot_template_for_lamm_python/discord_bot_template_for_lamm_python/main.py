6657b8053ea892710b220a56789b16080386bb70
antonpichka <antonpichka@gmail.com>
2024-06-29 13:06:46 +0300

    refactoring(.): release v3.0.0

--- discord_bot_template_for_lamm_python/discord_bot_template_for_lamm_python/main.py
+++ discord_bot_template_for_lamm_python/discord_bot_template_for_lamm_python/main.py
@@ -75,4 +75,4 @@ async def __kirill_w_init_w_build_w_callback_w_success(_: commands.Context, msg:
 
 ## Add a bot: https://discord.com/oauth2/authorize?client_id=1244168154658242580&permissions=8&scope=bot
 ## Remove two "QQ" on the left and right
-bot.run("MTI0NDE2ODE1NDY1ODI0MjU4MA.Gwzwjs.yvR7nvj9lkKKZ4xBdoOUXISzGKoGplZFXMMDWQ")
+bot.run("QQ_MTI0NDE2ODE1NDY1ODI0MjU4MA.Gwzwjs.yvR7nvj9lkKKZ4xBdoOUXISzGKoGplZFXMMDWQ_QQ")
